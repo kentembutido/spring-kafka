9a49264c3a03ab705f1cd15f1a5966f78c55cf77
kent.david.embile <kent.david.embile@accenture.com>
2023-04-12 20:44:50 +0800

    Initial commit

---
+++ demo-spring-kafka/src/main/java/com/kentisthebest/springkafka/DemoSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest.springkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringKafkaApplication.class, args);
+	}
+
+}
---
+++ demo-spring-kafka/src/test/java/com/kentisthebest/springkafka/DemoSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentisthebest.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
